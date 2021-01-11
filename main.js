b948b7280599057a8c2050c66b92ce0b5c732354
nixon666 <71195946+nixon666@users.noreply.github.com>
2021-01-11 15:04:33 -0500

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector('body')
-const burger = document.querySelector('.menu__burger');
-const menu = document.querySelector('.nav__menu');
-
-burger.addEventListener('click', function () {
-    burger.classList.toggle('active');
-    menu.classList.toggle('active');
-    body.classList.toggle('lock');
-});
